85da463b453c8629a3d5903ad59d386d8a9c902c
soban khan <sobankhan252@gmail.com>
2020-09-08 15:58:42 +0500

    added some file

---
+++ precache-manifest.634279d9bc300bdf5ce96fc0177bf9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177bb05ba2320ac9f7ac3c10e6862837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f1ccb8406ec59df672b",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1ccb8406ec59df672b",
+    "url": "/static/js/main.28470d3b.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
